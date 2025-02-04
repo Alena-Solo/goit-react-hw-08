3692a4e9018c683937cb77fd04871bf2bcd94dfa
Alena-Solo <bazarkina.alena2020@gmail.com>
2025-02-04 22:06:17 +0100

    nav

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,7 +1,7 @@
 import Contact from "../Contact/Contact";
 import s from "./ContactList.module.css";
 import { useSelector } from "react-redux";
-import { selectFilteredContacts } from "../../redux/selectors";
+import { selectFilteredContacts } from "../../redux/filters/selectors";
 
 export default function ContactList() {
   const filteredContacts = useSelector(selectFilteredContacts);
